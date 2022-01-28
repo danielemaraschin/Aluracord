03791b324868390ed3fd272ec5f571b47bd248c3
danielemaraschin <danikmaraschin@gmail.com>
2022-01-28 15:01:16 -0300

    style jsx

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63e827767e96717b"; }
+/******/ 		__webpack_require__.h = function() { return "42ecc6c52f4de12c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+function Titulo(){
+    return(
+        <h1>Boas vindas de volta</h1>
+    );
+}
+
+//component react
 function HomePage() {
     return (
-        <div>Welcome to Aluracord!</div>
+        <div>
+            <Titulo>testando 123</Titulo>
+            <h2>Discord - Alura Matrix</h2>
+            <style jsx>{`
+                h1{
+                    color: red;
+                }
+            `}</style>
+        </div>
     )
   }
   
