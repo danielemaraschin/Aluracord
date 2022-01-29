b831e8fbb8ac5e1e188adc61b3272b3e6497ccf9
danielemaraschin <danikmaraschin@gmail.com>
2022-01-28 21:51:02 -0300

    working with  skunexui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dfb32f8fdbaa3a82"; }
+/******/ 		__webpack_require__.h = function() { return "e7f7520eede21a92"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.4a1db239f19700da.hot-update.js
+++ .next/static/webpack/webpack.4a1db239f19700da.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d6d593ba698cd58a"; }
+/******/ 	__webpack_require__.h = function() { return "e7f7520eede21a92"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
