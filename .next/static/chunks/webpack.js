c549e37c5a76ce216f8c09dc2d3575b610f6870e
danielemaraschin <danikmaraschin@gmail.com>
2022-01-28 22:16:45 -0300

    working with  skunexui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca2ed5d6b2d95f00"; }
+/******/ 		__webpack_require__.h = function() { return "df6b4354cef66fca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ function GlobalStyle() {
 }
 
 function Titulo(props) {
-  const Tag = props.tag || 'h1';
+  const Tag = props.tag || 'h1'; //se nao passarem nenhum valor pela props.tag, então assume valor de h1
   return (
     <>
       <Tag>{props.children}</Tag>
