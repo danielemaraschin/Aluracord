35ab0ff36077c24a835b3e1b56db34c9fc9331c8
danielemaraschin <danikmaraschin@gmail.com>
2022-01-31 19:38:13 -0300

    Working with supabase

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c67d1a794af9b04e"; }
+/******/ 		__webpack_require__.h = function() { return "e3df58677c6d544f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.098144fefd1e18f4.hot-update.js
+++ .next/static/webpack/webpack.098144fefd1e18f4.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3814ef0d11b35439"; }
+/******/ 	__webpack_require__.h = function() { return "e3df58677c6d544f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
