2638b2a1015604b5278d19b7e9e20113bb3aac1a
danielemaraschin <danikmaraschin@gmail.com>
2022-01-31 18:15:21 -0300

    Working with supabase

--- pages/chat.js
+++ pages/chat.js
@@ -1,6 +1,7 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2MDA3NiwiZXhwIjoxOTU5MjM2MDc2fQ._WPZ6wTRt9N44MvecppoHk0EiGRJAym2gm30LR87H54'
 const SUPABASE_URL = 'https://icscbahdmnirzqegnofj.supabase.co'
